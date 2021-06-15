9d61c811538c02108866d3581e1b188f3290cb32
Alessandro Nardin <ale.daluch@gmail.com>
2021-06-15 14:32:09 +0200

    Code cleanup : renamed constants and variables in order to make them fit the standards.

--- src/Stain.Stage.ScreenshotUploader.Screenshot/Screenshot.cs
+++ src/Stain.Stage.ScreenshotUploader.Screenshot/Screenshot.cs
@@ -12,37 +12,37 @@ namespace Stain.Stage.ScreenshotUploader.Screenshot {
         static extern IntPtr GetConsoleWindow();
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-        const int SW_HIDE = 0;
-        const int SW_SHOW = 5;
+        const int SwHide = 0;
+        const int SwShow = 5;
 
         /// <summary>
         /// Returns a bitmap object containing a screenshot of the entire screen.
         /// </summary>
         public static string Capture(){
             // Creates the default points necessary to determine the screen size, and the starting point on the destination image.
-            Point ImageDestinationPoint = new Point(0, 0);
-            Point UpperLeftCorner = new Point(0, 0);
-            Point BottomRightCorner = new Point(Convert.ToInt32(SystemParameters.PrimaryScreenWidth), Convert.ToInt32(SystemParameters.PrimaryScreenHeight));
+            Point imageDestinationPoint = new Point(0, 0);
+            Point upperLeftCorner = new Point(0, 0);
+            Point bottomRightCorner = new Point(Convert.ToInt32(SystemParameters.PrimaryScreenWidth), Convert.ToInt32(SystemParameters.PrimaryScreenHeight));
 
             // Determines the size of the screen in pixel
-            int SizeWidth = BottomRightCorner.X - UpperLeftCorner.X;
-            int SizeHeigth = BottomRightCorner.Y - UpperLeftCorner.Y;
-            Size dimension = new Size(SizeWidth, SizeHeigth);
+            int sizeWidth = bottomRightCorner.X - upperLeftCorner.X;
+            int sizeHeigth = bottomRightCorner.Y - upperLeftCorner.Y;
+            Size dimension = new Size(sizeWidth, sizeHeigth);
 
             // Hides the console Window
             var handle = GetConsoleWindow();
-            ShowWindow(handle, SW_HIDE);
+            ShowWindow(handle, SwHide);
 
             // Takes the screenshot
-            Bitmap bm = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
-            Graphics g = Graphics.FromImage(bm);
-            g.CopyFromScreen(UpperLeftCorner, ImageDestinationPoint, dimension);
+            Bitmap screenshot = new Bitmap(sizeWidth,sizeHeigth);
+            Graphics graph = Graphics.FromImage(screenshot);
+            graph.CopyFromScreen(upperLeftCorner, imageDestinationPoint, dimension);
 
             // Shows the console Window
-            ShowWindow(handle, SW_SHOW);
+            ShowWindow(handle, SwShow);
 #if DEBUG
             string path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), $"{Guid.NewGuid()}.jpg");
-            bm.Save(@path);
+            screenshot.Save(@path);
             return path;
 #else
             string path = System.IO.Path.Combine(System.IO.Path.GetTempPath(), $"{Guid.NewGuid()}.jpg");
@@ -54,31 +54,31 @@ namespace Stain.Stage.ScreenshotUploader.Screenshot {
         /// <summary>
         /// Returns a bitmap object containing a screenshot of a portion of the screen, determined by the parameters.
         /// </summary>
-        /// <param name="UpperLeftCorner">Upper left corner of the part of the screen to capture.</param>
-        /// <param name="BottomRightCorner">Bottom right corner of the part of the screen to capture.</param>
-        public static string Capture(Point UpperLeftCorner, Point BottomRightCorner) {
+        /// <param name="upperLeftCorner">Upper left corner of the part of the screen to capture.</param>
+        /// <param name="bottomRightCorner">Bottom right corner of the part of the screen to capture.</param>
+        public static string Capture(Point upperLeftCorner, Point bottomRightCorner) {
             // Creates the default point necessary to determine the starting point on the destination image.
             Point ImageDestinationPoint = new Point(0, 0);
 
             // Determines the size of the screen in pixel
-            int SizeWidth = BottomRightCorner.X - UpperLeftCorner.X;
-            int SizeHeigth = BottomRightCorner.Y - UpperLeftCorner.Y;
-            Size dimension = new Size(SizeWidth, SizeHeigth);
+            int sizeWidth = bottomRightCorner.X - upperLeftCorner.X;
+            int sizeHeigth = bottomRightCorner.Y - upperLeftCorner.Y;
+            Size dimension = new Size(sizeWidth, sizeHeigth);
 
             // Hides the console Window
             var handle = GetConsoleWindow();
-            ShowWindow(handle, SW_HIDE);
+            ShowWindow(handle, SwHide);
 
             // Takes the screenshot
-            Bitmap bm = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
-            Graphics g = Graphics.FromImage(bm);
-            g.CopyFromScreen(UpperLeftCorner, ImageDestinationPoint, dimension);
+            Bitmap screenshot = new Bitmap(sizeWidth, sizeHeigth);
+            Graphics graph = Graphics.FromImage(screenshot);
+            graph.CopyFromScreen(upperLeftCorner, ImageDestinationPoint, dimension);
 
             // Shows the console Window
-            ShowWindow(handle, SW_SHOW);
+            ShowWindow(handle, SwShow);
 #if DEBUG
             string path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), $"{Guid.NewGuid()}.jpg");
-            bm.Save(@path);
+            screenshot.Save(@path);
             return path;
 #else
             string path = System.IO.Path.Combine(System.IO.Path.GetTempPath(), $"{Guid.NewGuid()}.jpg");
--- src/Stain.Stage.ScreenshotUploader/Program.cs
+++ src/Stain.Stage.ScreenshotUploader/Program.cs
@@ -9,7 +9,9 @@ namespace Stain.Stage.ScreenshotUploader {
 
         public static void Main(string[] args) {
             //Captures a screenshot and edits it with paint.
-            string screenshot = Screenshot.Screenshot.Capture();
+            Point p1 = new Point(100, 100);
+            Point p2 = new Point(500, 500);
+            string screenshot = Screenshot.Screenshot.Capture(p1,p2);
             Bitmap editedImage = Screenshot.ImageEditor.PaintEdit(screenshot);
 #if DEBUG 
             //Save the edited image on the desktop for test purposes.
