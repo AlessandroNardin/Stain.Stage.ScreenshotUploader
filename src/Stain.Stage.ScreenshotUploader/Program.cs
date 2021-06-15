dc8611dfed4dff6982a883566fbbcda964c563c6
Alessandro Nardin <ale.daluch@gmail.com>
2021-06-15 15:22:11 +0200

    Modified the types of some method and their parameters in order to make them more logical.

--- src/Stain.Stage.ScreenshotUploader/Program.cs
+++ src/Stain.Stage.ScreenshotUploader/Program.cs
@@ -11,7 +11,7 @@ namespace Stain.Stage.ScreenshotUploader {
             //Captures a screenshot and edits it with paint.
             Point p1 = new Point(100, 100);
             Point p2 = new Point(500, 500);
-            string screenshot = Screenshot.Screenshot.Capture(p1,p2);
+            Bitmap screenshot = Screenshot.Screenshot.Capture(p1,p2);
             Bitmap editedImage = Screenshot.ImageEditor.PaintEdit(screenshot);
 #if DEBUG 
             //Save the edited image on the desktop for test purposes.
