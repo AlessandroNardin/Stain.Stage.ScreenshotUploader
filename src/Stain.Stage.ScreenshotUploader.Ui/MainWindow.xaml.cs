7522427a286bbec9ccf9706a2365e95b62ec1958
Alessandro Nardin <ale.daluch@gmail.com>
2021-06-16 12:19:29 +0200

    "Edit" Button Now Working.

--- src/Stain.Stage.ScreenshotUploader.Ui/MainWindow.xaml.cs
+++ src/Stain.Stage.ScreenshotUploader.Ui/MainWindow.xaml.cs
@@ -7,27 +7,29 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Stain.Stage.ScreenshotUploader.Ui {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
-
         Bitmap screenshot;
         string path;
+
         public MainWindow() {
             InitializeComponent();
         }
 
         private void newScreenshot_Click(object sender, RoutedEventArgs e) {
             screenshot = Screenshot.Screenshot.Capture();
+            path = System.IO.Path.Combine(System.IO.Path.GetTempPath(), $"{Guid.NewGuid()}.jpg");
+            screenshot.Save(path);
+            ScreenshotPreview.Source = new BitmapImage(new Uri(path));
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e) {
+            screenshot = Screenshot.ImageEditor.PaintEdit(screenshot);
             path = System.IO.Path.Combine(System.IO.Path.GetTempPath(), $"{Guid.NewGuid()}.jpg");
             screenshot.Save(path);
             ScreenshotPreview.Source = new BitmapImage(new Uri(path));
