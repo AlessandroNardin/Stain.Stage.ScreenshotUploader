d193f747a64c0163e842a17104be8b74f8270f7d
Alessandro Nardin <ale.daluch@gmail.com>
2021-06-17 12:05:31 +0200

    Cleaned MainWindow.xaml.cs

--- src/Stain.Stage.ScreenshotUploader.Ui/MainWindow.xaml.cs
+++ src/Stain.Stage.ScreenshotUploader.Ui/MainWindow.xaml.cs
@@ -10,12 +10,13 @@ namespace Stain.Stage.ScreenshotUploader.Ui {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : INotifyPropertyChanged{
-        private Bitmap _screenShot;
-
+        private Bitmap imageBitmap;
         private string imagePath = @"C:\Users\utente.elettrico.STAIN\source\repos\Stain.Stage.ScreenshotUploader\Default.png";
         private string uploadedScreenshotLink;
         private bool isScreenshotTaken = false;
         private bool isUploaded = false;
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public string ImagePath {
             get { return imagePath; }
             set {
@@ -52,35 +53,37 @@ namespace Stain.Stage.ScreenshotUploader.Ui {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(arg));
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
         public MainWindow() {
             InitializeComponent();
             DataContext = this;
         }
 
         private void newScreenshot_Click(object sender, RoutedEventArgs e) {
-            this.WindowState = WindowState.Minimized;
+            WindowState = WindowState.Minimized;
             Thread.Sleep(250);
-            _screenShot = Screenshot.Screenshot.Capture();
-            this.WindowState = WindowState.Normal;
+            imageBitmap = Screenshot.Screenshot.Capture();
+            WindowState = WindowState.Normal;
+
             string tempPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), $"{Guid.NewGuid()}.png");
-            _screenShot.Save(tempPath);
+            imageBitmap.Save(tempPath);
             ImagePath = tempPath;
+
             IsScreenshotTaken = true;
         }
 
         private void Edit_Click(object sender, RoutedEventArgs e) {
-            _screenShot = Screenshot.ImageEditor.PaintEdit(_screenShot);
+            imageBitmap = Screenshot.ImageEditor.PaintEdit(imageBitmap);
+
             string tempPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), $"{Guid.NewGuid()}.png");
-            _screenShot.Save(tempPath);
+            imageBitmap.Save(tempPath);
             ImagePath = tempPath;
         }
 
         private void Upload_Click(object sender, RoutedEventArgs e) {
             UploadData data;
-            UploadFile.Instance.TryUploadImage(_screenShot, out data);
+            UploadFile.Instance.TryUploadImage(imageBitmap, out data);
             UploadedScreenshotLink = data.Link;
+
             IsUploaded = true;
         }
 
